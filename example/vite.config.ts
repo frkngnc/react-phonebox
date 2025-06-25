cb49699980fe85ed2b6463c20cf82a1cd0a9a0ca
Furkan Genç <furkan.genc@trtworld.com>
2025-06-25 20:22:58 +0300

    update

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -1,13 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      "react-phonebox": path.resolve(__dirname, "../src")
-    }
-  },
-  root: __dirname
+  build: {
+    outDir: "dist",
+  }
 });
