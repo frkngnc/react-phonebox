490ffa8c2f99b0a1735c784a7bd5a4416719e33c
Furkan Genç <furkan.genc@trtworld.com>
2025-06-28 05:49:54 +0300

    fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { fileURLToPath } from 'url';
-import path from 'path';
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { fileURLToPath } from "url";
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -10,24 +10,37 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@components': path.resolve(__dirname, './src/components'),
-      '@utils': path.resolve(__dirname, './src/utils')
-    }
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@utils": path.resolve(__dirname, "./src/utils"),
+    },
   },
   build: {
     lib: {
-      entry: 'src/index.ts',
-      name: 'ReactPhoneBox',
-      fileName: (format) => `index.${format}.js`
+      entry: "src/index.ts",
+      name: "ReactPhoneBox",
+      formats: ["es", "umd", "cjs"],
+      fileName: (format) => {
+        switch (format) {
+          case "es":
+            return "index.es.js";
+          case "umd":
+            return "index.umd.js";
+          case "cjs":
+            return "index.cjs.js";
+          default:
+            return `index.${format}.js`;
+        }
+      },
     },
     rollupOptions: {
-      external: ['react', 'react-dom'],
+      external: ["react", "react-dom", "react/jsx-runtime"],
       output: {
         globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM'
-        }
-      }
-    }
-  }
+          react: "React",
+          "react-dom": "ReactDOM",
+          "react/jsx-runtime": "jsxRuntime",
+        },
+      },
+    },
+  },
 });
